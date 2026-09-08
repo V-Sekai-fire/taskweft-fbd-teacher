6b7bfb31016d983a1e8b5d8fb7e86c2e0dd09791
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-09-08 11:42:04 -0700

    Keep the shell-script line in .gitattributes beside the workspace-wide one

--- scripts/deps_check.exs
+++ scripts/deps_check.exs
@@ -1,7 +1,14 @@
 alias Explorer.DataFrame, as: DF
 alias Explorer.Series
 png = Series.from_list([<<137, 80, 78, 71, 0, 255>>], dtype: :binary)
-df = DF.new(sub_id: Series.from_list([24], dtype: {:s, 16}), trial: Series.from_list([1], dtype: {:s, 8}), png: png)
+
+df =
+  DF.new(
+    sub_id: Series.from_list([24], dtype: {:s, 16}),
+    trial: Series.from_list([1], dtype: {:s, 8}),
+    png: png
+  )
+
 path = Path.join(System.tmp_dir!(), "fbd_roundtrip.parquet")
 :ok = DF.to_parquet(df, path, compression: {:zstd, 3})
 back = DF.from_parquet!(path)
